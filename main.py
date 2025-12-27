68843c3958d0bce78fe1e9905fadf1d5dd8878aa
Peter Santesson <santessonpeter97@gmail.com>
2025-12-27 18:36:45 +0100

    update scoring, improve performance and up the tickrate

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+from datetime import datetime
 import os
 import random
+from time import sleep
 from pynput import keyboard
 
-TICKRATE = 24
+TICKRATE = 60
 WIDTH = 10
 HEIGHT = 22
 SPAWN_COORDINATES = (2, 5)
 SPAWN_ROTATION_INDEX = 0
+DROP_RATES = [60, 30, 20, 15, 12, 10, 6, 5, 4, 3, 2, 1]
+SCORE_PER_LINE_CLEARED = [0, 40, 100, 300, 1200]
 
 DIRECTIONS = {
     'left': (0, -1),
@@ -64,13 +68,13 @@ PIECES = [
     LINE_PIECE_ROTATIONS
 ]
 
-DROP_RATES = [24, 12, 8, 6, 4, 3, 2, 1]
 
 class Game:
-    def __init__(self):
+    def __init__(self, start_drop_rate):
         self.board = [[' '] * WIDTH*2 for _ in range(HEIGHT)]
         self.coordinates = SPAWN_COORDINATES
         self.rotation_index = SPAWN_ROTATION_INDEX
+        self.start_drop_rate = start_drop_rate
         self.tick = 0
         self.drop_rate_index = 0
         self.lines_cleared = 0
@@ -85,7 +89,7 @@ class Game:
     
     def increment(self):
         self.tick = (self.tick+1) % TICKRATE
-        self.drop_rate_index = self.lines_cleared//2
+        self.drop_rate_index = max(self.start_drop_rate, self.lines_cleared//10)
 
         if self.tick % DROP_RATES[self.drop_rate_index] == 0:
             self.move_piece('down')
@@ -128,7 +132,7 @@ class Game:
         
         self.coordinates = (row, col)
 
-    def rotate_piece(self):
+    def rotate_piece(self, direction: int):
         row, col = self.coordinates
 
         for row_mod, col_mod in PIECES[self.piece_index][self.rotation_index]:
@@ -136,7 +140,7 @@ class Game:
             self.board[row+row_mod][(col+col_mod)*2+1] = ' '
 
         rotations = len(PIECES[self.piece_index])
-        new_rotation = (self.rotation_index+1) % rotations
+        new_rotation = (self.rotation_index+direction) % rotations
 
         for row_mod, col_mod in PIECES[self.piece_index][new_rotation]:
             if (row+row_mod >= HEIGHT
@@ -176,14 +180,17 @@ class Game:
 
     def on_press(self, key: keyboard.Key | keyboard.KeyCode):
         if isinstance(key, keyboard.KeyCode):
+            if key.char == 'z':
+                self.rotate_piece(-1)
+            if key.char == 'x':
+                self.rotate_piece(1)
             if key.char == 'q':
                 self.over = True
-            return
-
-        if key.name in ('left', 'right', 'down'):
-            self.move_piece(key.name)
-        if key.name == 'up':
-            self.rotate_piece()
+        else:
+            if key.name in ('left', 'right', 'down'):
+                self.move_piece(key.name)
+            if key.name == 'up':
+                self.rotate_piece(1)
     
     def render(self):
         for i in range(2, len(self.board)):
@@ -213,29 +220,45 @@ class Game:
         print(' ' + 'v' * len(self.board[0]) + ' '*3 + 'points: ' + str(self.points) + ' lines cleared: ' + str(self.lines_cleared))
 
     def clear_lines(self):
-        points = 0
+        lines_cleared = 0
         for row in range(HEIGHT):
             if ' ' not in self.board[row]:
                 self.board = [[' '] * WIDTH*2] + self.board[:row] + self.board[row+1:]
-                points *= 3
-                points += 10
-                self.lines_cleared += 1
-        self.points += points
+                lines_cleared += 1
+        self.points += SCORE_PER_LINE_CLEARED[lines_cleared]
+        self.lines_cleared += lines_cleared
 
 def main():
-    game = Game()
+    while True:
+        start_drop_rate = -1
+        while start_drop_rate not in range(12):
+            start_drop_rate = int(input('Enter starting difficulty (1-12): '))-1
 
-    listener = keyboard.Listener(on_press=game.on_press)
-    listener.start()
+        game = Game(start_drop_rate)
 
-    while not game.over:
-        listener.join(1/TICKRATE)
+        listener = keyboard.Listener(on_press=game.on_press)
+        listener.start()
 
-        game.increment()
-        os.system('clear')
-        game.render()
-    
-    print('GAME OVER')
-    listener.stop()
+        if os.name == 'nt':
+            os.system('cls')
+        else:
+            os.system('clear')
+
+        while not game.over:
+            before = datetime.now().timestamp()
+
+            game.increment()
+            print("\033[H", end="")
+            game.render()
+
+            after = datetime.now().timestamp()
+            time_elapsed = after-before
+            sleep(max((1/TICKRATE)-time_elapsed, 0))
+        
+        listener.stop()
+        print('GAME OVER')
+        
+        if input('Play again? (*/n):') == 'n':
+            break
 
 main()
